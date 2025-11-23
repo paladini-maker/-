ccf738a89c3004d1a3201b630f80e3f35277cd0a
paladini-maker <paladinifranco93@gmail.com>
2025-11-23 20:55:56 -0300

    Actualizar myapp.js

--- myapp.js
+++ myapp.js
@@ -1,55 +1,44 @@
 const manifestUri =
-    'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd';
+  'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd';
 
 async function init() {
-  // When using the UI, the player is made automatically by the UI object.
   const video = document.getElementById('video');
-  const ui = video['ui'];
+
+  // UI de Shaka ya creó el player internamente
+  const ui = video.ui;
   const controls = ui.getControls();
   const player = controls.getPlayer();
 
-  // Attach player and UI to the window to make it easy to access in the JS console.
   window.player = player;
   window.ui = ui;
 
-  // Listen for error events.
   player.addEventListener('error', onPlayerErrorEvent);
   controls.addEventListener('error', onUIErrorEvent);
 
-  // Try to load a manifest.
-  // This is an asynchronous process.
   try {
     await player.load(manifestUri);
-    // This runs if the asynchronous load is successful.
-    console.log('The video has now been loaded!');
+    console.log('El video cargó correctamente!');
   } catch (error) {
     onPlayerError(error);
   }
 }
 
-function onPlayerErrorEvent(errorEvent) {
-  // Extract the shaka.util.Error object from the event.
+function onPlayerErrorEvent(event) {
   onPlayerError(event.detail);
 }
 
 function onPlayerError(error) {
-  // Handle player error
-  console.error('Error code', error.code, 'object', error);
+  console.error('Player Error:', error.code, error);
 }
 
-function onUIErrorEvent(errorEvent) {
-  // Extract the shaka.util.Error object from the event.
+function onUIErrorEvent(event) {
   onPlayerError(event.detail);
 }
 
 function initFailed(errorEvent) {
-  // Handle the failure to load; errorEvent.detail.reasonCode has a
-  // shaka.ui.FailReasonCode describing why.
-  console.error('Unable to load the UI library!');
+  console.error('No se pudo cargar la UI de Shaka!', errorEvent.detail);
 }
 
-// Listen to the custom shaka-ui-loaded event, to wait until the UI is loaded.
+// Eventos de carga de la UI
 document.addEventListener('shaka-ui-loaded', init);
-// Listen to the custom shaka-ui-load-failed event, in case Shaka Player fails
-// to load (e.g. due to lack of browser support).
 document.addEventListener('shaka-ui-load-failed', initFailed);
